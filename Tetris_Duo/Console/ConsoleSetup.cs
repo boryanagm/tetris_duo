c5e7c3fda38892a115fb21984d23bc0cd8708c04
Niki <origamista99@gmail.com>
2020-11-28 17:33:13 +0200

    Initialize the console related classes

---
+++ Tetris_Duo/Console/ConsoleSetup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class ConsoleSetup
+{
+    public static void InitialConsoleSetup()
+    {
+        Console.Title = "Duo Tetris"; // CONSOLE_TITLE
+
+        Console.WindowWidth = 40; // CONSOLE_WIDTH
+        Console.WindowHeight = 30; // CONSOLE_HEIGHT
+        Console.BufferWidth = 40; // CONSOLE_WIDTH
+        Console.BufferHeight = 30; // CONSOLE_HEIGHT
+
+        Console.CursorVisible = false;
+    }
+}
