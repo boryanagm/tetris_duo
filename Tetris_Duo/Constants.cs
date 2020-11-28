ed71f8a76bf6bdd3c72b54624f86211f5c267044
boryana.gm <boryana.gm@gmail.com>
2020-11-28 17:38:54 +0200

    Added Constants file

---
+++ Tetris_Duo/Constants.cs
@@ -0,0 +1,13 @@
+﻿
+public static class Constants
+
+{
+    public const char BOLD_BLOCK = '█';
+
+    public const char LOW_DENSITY_DOTTED = '░';
+
+    public const char MEDIUM_DENSITY_DOTTED = '▒';
+
+    public const char HIGH_DENSITY_DOTTED = '▓';
+
+}
