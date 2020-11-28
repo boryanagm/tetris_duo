b89a8f209270e9800e0ea517c471bfa18b46ea7d
Niki <origamista99@gmail.com>
2020-11-28 20:14:40 +0200

    Bo's homework "Corners & Border setup"

--- Tetris_Duo/Console/ConsoleSetup.cs
+++ Tetris_Duo/Console/ConsoleSetup.cs
@@ -6,12 +6,12 @@ public static class ConsoleSetup
 {
     public static void InitialConsoleSetup(GameMatrix matrix)
     {
-        Console.Title = CONSOLE_TITLE; 
+        Console.Title = CONSOLE_TITLE;
 
-        Console.WindowWidth = CONSOLE_WINDOW_WIDTH; 
-        Console.WindowHeight = CONSOLE_WINDOW_HEIGH; 
-        Console.BufferWidth = CONSOLE_BUFFER_WIDTH; 
-        Console.BufferHeight = CONSOLE_BUFFER_HEIGHT; 
+        Console.WindowWidth = CONSOLE_WINDOW_WIDTH;
+        Console.WindowHeight = CONSOLE_WINDOW_HEIGH;
+        Console.BufferWidth = CONSOLE_BUFFER_WIDTH;
+        Console.BufferHeight = CONSOLE_BUFFER_HEIGHT;
 
         Console.CursorVisible = false;
 
@@ -19,6 +19,41 @@ public static class ConsoleSetup
     }
 
     private static void SetupBorder(GameMatrix matrix)
+    {
+        DrawBorder(matrix);
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        DrawCorners(matrix);
+    }
+
+    private static void DrawBorder(GameMatrix matrix)
+    {
+        //for (int col = -1; col <= MATRIX_WIDTH + 1; col++)
+        //{
+        //    Console.SetCursorPosition(MATRIX_TOP_LEFT_CORNER_X + col, MATRIX_TOP_LEFT_CORNER_Y - 1);
+        //    Console.Write(BOLD_BLOCK);
+        //}
+        
+        //for (int row = -1; row <= MATRIX_HEIGHT + 1; row++)
+        //{
+        //    Console.SetCursorPosition(MATRIX_TOP_LEFT_CORNER_X - 1, MATRIX_TOP_LEFT_CORNER_Y + row);
+        //    Console.Write(BOLD_BLOCK);
+        //}
+        
+        //for (int col = -1; col <= MATRIX_WIDTH + 1; col++)
+        //{
+        //    Console.SetCursorPosition(MATRIX_TOP_LEFT_CORNER_X + col, MATRIX_HEIGHT + MATRIX_TOP_LEFT_CORNER_Y + 1);
+        //    Console.Write(BOLD_BLOCK);
+        //}
+        
+        //for (int row = -1; row <= MATRIX_HEIGHT + 1; row++)
+        //{
+        //    Console.SetCursorPosition(MATRIX_WIDTH + MATRIX_TOP_LEFT_CORNER_X + 1, MATRIX_TOP_LEFT_CORNER_Y + row);
+        //    Console.Write(BOLD_BLOCK);
+        //}
+    }
+
+    private static void DrawCorners(GameMatrix matrix)
     {
         Console.SetCursorPosition(matrix.TopLeftCorner.X, matrix.TopLeftCorner.Y);
         Console.Write(BOLD_BLOCK);
@@ -31,30 +66,5 @@ public static class ConsoleSetup
 
         Console.SetCursorPosition(matrix.BottomRightCorner.X, matrix.BottomRightCorner.Y);
         Console.Write(BOLD_BLOCK);
-
-
-        //for (int col = matrix.TopLeftCorner.Y; col <= matrix.TopRightCorner.X; col++)
-        //{
-        //    Console.SetCursorPosition(matrix.TopLeftCorner.Y + col, matrix.TopRightCorner.X);
-        //    Console.Write(BOLD_BLOCK);
-        //}
-
-        //for (int row = -1; row <= MATRIX_HEIGHT + 1; row++)
-        //{
-        //    Console.SetCursorPosition(MATRIX_TOP_LEFT_CORNER - 1, MATRIX_TOP_LEFT_CORNER_Y + row);
-        //    Console.Write(BOLD_BLOCK);
-        //}
-
-        //for (int col = -1; col <= MATRIX_WIDTH + 1; col++)
-        //{
-        //    Console.SetCursorPosition(MATRIX_TOP_LEFT_CORNER + col, MATRIX_HEIGHT + MATRIX_TOP_LEFT_CORNER_Y + 1);
-        //    Console.Write(BOLD_BLOCK);
-        //}
-
-        //for (int row = -1; row <= MATRIX_HEIGHT + 1; row++)
-        //{
-        //    Console.SetCursorPosition(MATRIX_WIDTH + MATRIX_TOP_LEFT_CORNER + 1, MATRIX_TOP_LEFT_CORNER_Y + row);
-        //    Console.Write(BOLD_BLOCK);
-        //}
     }
 }
